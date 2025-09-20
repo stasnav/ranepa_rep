489a04d72d0ef9b409cbc84bf4b258d276748a81
Stas <stas@MacBook-Air-Stas.local>
2025-09-20 11:10:59 +0300

    5. Создание собственного модуля my_math

--- my_math.py
+++ my_math.py
@@ -1,12 +1,12 @@
-def my_factorial(n):
-    if n < 0:
+def my_factorial(k):
+    if k < 0:
         raise ValueError("Факториал определен только для неотрицательных целых чисел")
     
-    if n == 0 or n == 1:
+    if k == 0 or k == 1:
         return 1
     
     result = 1
-    for i in range(2, n + 1):
+    for i in range(2, k + 1):
         result *= i
     
     return result
